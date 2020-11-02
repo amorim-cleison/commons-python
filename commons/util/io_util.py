5f6c068b2fec823de470c935eb90bdd70c72829c
Cleison Correia de Amorim <cleison.c.amorim@accenture.com>
2020-11-02 12:10:01 -0300

    Bug fix in io_utils

--- commons/util/io_util.py
+++ commons/util/io_util.py
@@ -1,51 +1,52 @@
 NEW_LINE = "\n"
 
 
-def filter_files(dir, name="*", ext="*", recursive=False):
+def filter_files(dir, name="*", ext="*", recursive=False, as_str=True):
     """Filter the files in the directory, based on the name and extension provided"""
     assert (dir is not None), "`dir_path` is required"
     _ext = ext if ext.startswith(".") else f".{ext}"
 
     if recursive:
-        fn = __prepare_path(dir).rglob
+        fn = __get_path(dir).rglob
     else:
-        fn = __prepare_path(dir).glob
-    return fn(f"{name}{_ext}")
+        fn = __get_path(dir).glob
+    result = list(fn(f"{name}{_ext}"))
+    return __parse_result(result, as_str)
 
 
 def create_if_missing(dir):
     """Create directory if it does not exist"""
-    __prepare_path(dir).mkdir(parents=True, exist_ok=True)
+    __get_path(dir).mkdir(parents=True, exist_ok=True)
 
 
 def delete_dir(dir):
     """ Recursively remove a directory """
     from shutil import rmtree
-    _dir = __prepare_path(dir).as_posix()
+    _dir = normpath(dir)
     if exists(_dir):
         rmtree(_dir, ignore_errors=True)
 
 
 def exists(path):
-    return __prepare_path(path).exists()
+    return __get_path(path).exists()
 
 
 def is_file(path):
-    return exists(path) and __prepare_path(path).is_file()
+    return exists(path) and __get_path(path).is_file()
 
 
 def is_dir(path):
-    return exists(path) and __prepare_path(path).is_dir()
+    return exists(path) and __get_path(path).is_dir()
 
 
 def read_json(path_or_dir, include_path=False):
     import json
     data = None
-    _path = __prepare_path(path_or_dir)
+    _path = __get_path(path_or_dir)
 
     def read_single(path):
         """ Reads a single file """
-        with open(path) as file:
+        with path.open() as file:
             raw = json.load(file)
             return (raw, path) if include_path else raw
 
@@ -54,9 +55,9 @@ def read_json(path_or_dir, include_path=False):
         return [read_single(path) for path in paths]
 
     if _path.is_file():
-        data = read_single(_path.as_posix())
+        data = read_single(_path)
     elif _path.is_dir():
-        paths = filter_files(_path.as_posix(), ext="json")
+        paths = filter_files(_path, ext="json", as_str=False)
         data = read_multiple(paths)
     else:
         data = None
@@ -65,33 +66,33 @@ def read_json(path_or_dir, include_path=False):
 
 def read_items(path):
     """ Save the items into a file. """
-    with __prepare_path(path).open("r", newline=NEW_LINE) as file:
+    with __get_path(path).open("r", newline=NEW_LINE) as file:
         lines = file.readlines()
         return list(map(lambda x: x.replace(NEW_LINE, ""), lines))
 
 
 def read_yaml(path):
     import yaml
-    with __prepare_path(path).open('r') as file:
+    with __get_path(path).open('r') as file:
         return yaml.full_load(file)
 
 
 def save_json(data: dict, path: str):
     import json
-    with __prepare_path(path).open('w') as file:
+    with __get_path(path).open('w') as file:
         json.dump(data, file)
 
 
 def save_yaml(data: dict, path: str):
     import yaml
-    with __prepare_path(path).open('w') as file:
+    with __get_path(path).open('w') as file:
         yaml.dump(data, file, default_flow_style=False, indent=4)
 
 
 def save_items(items, path, append=False):
     """ Save the items into a file. """
     mode = "a" if append else "w"
-    with __prepare_path(path).open(mode, newline=NEW_LINE) as file:
+    with __get_path(path).open(mode, newline=NEW_LINE) as file:
         file.write(NEW_LINE.join(items))
 
 
@@ -106,16 +107,26 @@ def download_file(url, target_file):
 
 def filename(path, with_extension=True):
     from os.path import splitext, basename
-    filename = basename(__prepare_path(path))
+    filename = basename(normpath(path))
     if not with_extension:
         filename = splitext(filename)[0]
     return filename
 
 
-def normalize_path(path):
-    return __prepare_path(path).as_posix()
+def normpath(path, as_str=True):
+    return __parse_result(__get_path(path), as_str)
 
 
-def __prepare_path(path):
+def __get_path(path):
     from pathlib import Path
     return Path(path).expanduser().absolute()
+
+def __parse_result(path, as_str=True):
+    from pathlib import Path
+    if as_str:
+        if isinstance(path, list):
+            return [str(x) for x in path]
+        else:
+            return str(path)
+    else:
+        return path
